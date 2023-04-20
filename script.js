dcf0a1b230ccbca0cd04679d1559e2342fd59c08
Ritesh Sohan Sinha <sinharitesh919@gmail.com>
2023-04-21 00:25:14 +0530

    added font and setup the layout

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 const validateForm = () => {
     const name = document.forms["loginForm"]["userName"].value;
-    const password = document.forms["loginForm"]["userPassword"].value;
+    const password = document.forms["loginForm"]["userPassword"].value; 
 }
